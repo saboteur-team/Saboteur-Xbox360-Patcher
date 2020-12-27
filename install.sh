0cb539fb625db2906e2afe6072a52b6a126633c8
Daniele Scasciafratte <mte90net@gmail.com>
2020-12-27 16:30:25 +0100

    fix(unzip): overwrite

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ mkdir -p tools
 
 echo "Downloading Quickbms"
 wget https://aluigi.altervista.org/papers/quickbms_linux.zip > /dev/null
-unzip quickbms_linux.zip -d ./tools
+unzip -o quickbms_linux.zip -d ./tools
 chmod +x ./tools/quickbms
 rm quickbms_linux.zip
 
 echo "Downloading unluac.jar"
 wget https://kumisystems.dl.sourceforge.net/project/unluac/unluacv1.2.2.zip
-unzip unluacv1.2.2.zip -d ./tools
+unzip -o unluacv1.2.2.zip -d ./tools
 rm unluacv1.2.2.zip
 
 echo "Downloading luac5.1.exe"
-wget https://netcologne.dl.sourceforge.net/project/luabinaries/5.1.5/Tools%20Executables/lua-5.1.5_Win32_bin.zip
-unzip lua-5.1.5_Win32_bin.zip -d ./tools
+wget https://netcologne.dl.sourceforge.net/project/luabinaries/5.1.4/Tools%20Executables/lua-5.1.5_Win32_bin.zip
+unzip -o lua-5.1.5_Win32_bin.zip -d ./tools
 rm lua-5.1.5_Win32_bin.zip
